20ca493eabe1160770bd5aaf015da6f4717ee5b2
SmartSHEE <Edidiong Obodom>
2023-05-13 04:13:26 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
+$(document).ready(() => {
+  // gets the username from the input
+  const username = $("#username");
 
-$(document).ready(
-    () => {
-        const username = $("#username");
-        $("#fadeOut").click(() => {
-            $("#box").fadeOut(3000, () => {
-                $("#fadeOut").text("Tadah!");
-                $("#fadeIn").text("Learn More");
+  //   fade out animation on click of the download button
+  $("#fadeOut").click(() => {
+    $("#box").fadeOut(3000, () => {
+      $("#fadeOut").text("Tadah!");
+      $("#fadeIn").text("Learn More");
+    });
+  });
+  // fade in animation on click of the learn more button
+  $("#fadeIn").click(() => {
+    $("#box").fadeIn(3000, () => {
+      $("#fadeIn").text("Tadah!");
+      $("#fadeOut").text("Download");
+    });
+  });
 
-            });
-        });
-
-        $("#fadeIn").click(() => {
-            $("#box").fadeIn(3000, () => {
-                $("#fadeIn").text("Tadah!");
-                $("#fadeOut").text("Download");
-            });
-        });
-
-        $('#shower').fadeIn(2000).removeClass('hide');
-
-        $(".stuff").click(async() => {
-            await Cookies.remove("first-cookie");
-            sessionStorage.clear();
-            return (location.href = "signup.html");
-        })
-        $(".myform").submit(async() => {
-        await Cookies.set("first-cookie", username.val());
-         sessionStorage.setItem("first-cookie", username.val());
-        })
-    }
-);
+  //   fade in animation for the body
+  $("#shower").fadeIn(2000).removeClass("hide");
+  // logs user out
+  $(".stuff").click(async () => {
+    await Cookies.remove("first-cookie");
+    return (location.href = "signup.html");
+  });
+  //   signs user up/login
+  $(".myform").submit(async () => {
+    await Cookies.set("first-cookie", username.val());
+  });
+});
